bcd1be0d5f7f38ab1443c80d9e388e393dbbc4ae
demeritbird <lekjiewei@hotmail.sg>
2022-10-01 18:42:25 +0800

    initial commit: cilent/server

---
+++ cilent/src/components/Help.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Help() {
+  return <h1>HELP</h1>;
+}
+
+export default Help;
---
+++ server/app.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+export { app };
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Hello from the server side!');
+});
+
+app.use(express.static(`${__dirname}/../../public`));
+
+// import NextFunction --> next: NextFunction
