7f6a4e5d9d419100cddba0671adc6cc5d0e5f292
demeritbird <lekjiewei@hotmail.sg>
2022-10-11 20:19:50 +0800

    fix: Place cors in right file

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 export { app };
 
 const app: Express = express();
+
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello from the server side!');
