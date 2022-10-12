08874ec994f85e4ad67e05bece33735776d6a3d2
demeritbird <lekjiewei@hotmail.sg>
2022-10-12 17:30:11 +0800

    prod: Add more prod config

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: './../../config.env' });
 
-const PORT: number = 3001;
+const PORT: string | number = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(process.env.NODE_ENV);
   console.log(`App running on port ${PORT}...`);
