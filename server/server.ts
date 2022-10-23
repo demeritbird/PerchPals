8d8fba2e26f0b1896f12a0921eab03d36823a237
demeritbird <lekjiewei@hotmail.sg>
2022-10-23 16:17:27 +0800

    revert: remove lerna architecture

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './../../config.env' });
+dotenv.config({ path: './../config.env' });
 
 const PORT: string | number = process.env.PORT || 3001;
 app.listen(PORT, () => {
