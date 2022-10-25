632fd782086f25e1832fd09ce8c4033cff20d178
demeritbird <lekjiewei@hotmail.sg>
2022-10-25 20:13:54 +0800

    play: implement interface & types to states

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import Help from './components/Help';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 import { logValidity } from './utils/helpers/log.helpers';
 import { Validity } from './utils/constants/types.constants';
 
+type cool = {
+  title: string;
+};
+
+interface cool2 {
+  name: number;
+}
+
 function App() {
+  const [nice, setNice] = useState<cool | undefined>(undefined);
+  const [nice2, setNice2] = useState<cool2 | undefined>(undefined);
+
+  useEffect(() => {
+    setNice({
+      title: 'foo',
+    });
+    setNice2({
+      name: 2,
+    });
+  }, []);
+
+  console.log(nice, 'nice 1');
+  console.log(nice2, 'nice 2');
+
   logValidity(Validity.PASS, 'pass message!');
   logValidity(Validity.FAIL, 'fail message!');
-  
+
   async function getTestData() {
     await fetch(`${process.env.REACT_APP_LINK}/testdata`, {})
       .then((res) => res.json())
