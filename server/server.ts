1475d74534c771a740411236b556add137406267
demeritbird <lekjiewei@hotmail.sg>
2022-10-25 19:52:37 +0800

    feat: connect server to mongodb compass

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,15 @@
 import { app } from './app';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config({ path: './../config.env' });
 
+const DB: string = process.env.DATABASE!.replace('<PASSWORD>', process.env.DATABASE_PASSWORD!);
+mongoose.connect(DB).then((con) => {
+  console.log('DB connection successful!');
+});
+
 const PORT: string | number = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(process.env.NODE_ENV);
   console.log(`App running on port ${PORT}...`);
 });
