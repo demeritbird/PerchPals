2d88aa4ad8e8d77526121be6931017ee32099e4e
demeritbird <lekjiewei@hotmail.sg>
2022-10-28 23:32:22 +0800

    test: tweak client sample test to fit router

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('sample render test', () => {
+  render(<App />, { wrapper: BrowserRouter });
+  const textElement = screen.getByText(/username/i);
+  expect(textElement).toBeInTheDocument();
 });
