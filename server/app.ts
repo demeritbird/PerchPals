2c758109a24c2f902c0b6d46312a4dceac886ffd
demeritbird <lekjiewei@hotmail.sg>
2022-10-31 10:42:57 +0800

    chore: install morgan for dev logging

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 export { app };
 
 const app: Express = express();
 
 app.use(cors());
 app.options('*', cors());
+
+// Development Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send(`Hello from the server side ${process.env.NODE_ENV}!`);
