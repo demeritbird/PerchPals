59ca397b6e2a1f87421b5ffe0114a71d1c7d2313
demeritbird <lekjiewei@hotmail.sg>
2022-11-05 09:19:37 +0800

    prod: add redirects file to config netlify routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import Help from './components/Help';
 import './App.scss';
-
+import React, { Fragment, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import RequireAuth from './routes/RequireAuth';
-import LoginPage from './routes/LoginPage';
-import LandingPage from './routes/LandingPage';
 
 import { logValidity } from './utils/helpers/log.helpers';
 import { Validity, Roles } from './utils/constants/types.constants';
+
+const LoginPage = React.lazy(() => import('./routes/LoginPage'));
+const LandingPage = React.lazy(() => import('./routes/LandingPage'));
 
 function App() {
   logValidity(Validity.PASS, 'pass message!');
@@ -22,17 +23,28 @@ function App() {
   }
 
   return (
-    <>
+    <Fragment>
       <button onClick={getTestData}>test me</button>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/login' element={<LoginPage />} />
 
+        <Route
+          path='/publicpage'
+          element={
+            <Suspense fallback={<div>loading...</div>}>
+              <LandingPage />
+            </Suspense>
+          }
+        />
+
         <Route element={<RequireAuth allowedRoles={[Roles.USER, Roles.ADMIN]} />}>
           <Route path='/landingpage' element={<LandingPage />} />
         </Route>
+
+        <Route path='/*' element={<h4>error</h4>} />
       </Routes>
-    </>
+    </Fragment>
   );
 }
 
