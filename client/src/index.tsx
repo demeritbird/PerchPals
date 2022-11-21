f9a359293f46eb23c231564be1c5866a4d250328
demeritbird <lekjiewei@hotmail.sg>
2022-11-21 17:36:55 +0800

    prod: remove ReactDevTools in production

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthProvider';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+if (import.meta.env.VITE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
