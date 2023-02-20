a01e2fa471e2843524e300793fad35f78eb327ab
demeritbird <lekjiewei@hotmail.sg>
2023-02-20 14:43:37 +0800

    test: fix naive render test for bouncingcirclesloading

--- client/src/components/loadingstates/BouncingCirclesLoading/BouncingCirclesLoading.test.tsx
+++ client/src/components/loadingstates/BouncingCirclesLoading/BouncingCirclesLoading.test.tsx
@@ -4,9 +4,7 @@ import BouncingBallsLoading from './BouncingCirclesLoading';
 test('Display Correct Sized Loading Screen', () => {
   render(<BouncingBallsLoading size='large' />, {});
 
-  expect(screen.getByTestId('container')).toHaveClass(
-    `_container_3d0a03 _container--large_3d0a03`
-  );
-  expect(screen.getByTestId('ball')).toHaveClass(`_ball_3d0a03 _ball--large_3d0a03`);
-  expect(screen.getByTestId('shadow')).toHaveClass(`_shadow_3d0a03 _shadow--large_3d0a03`);
+  expect(screen.getByTestId(/container/i)).toBeInTheDocument();
+  expect(screen.getByTestId(/circle/i)).toBeInTheDocument();
+  expect(screen.getByTestId(/shadow/i)).toBeInTheDocument();
 });
