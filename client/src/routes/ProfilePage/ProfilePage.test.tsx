cdd5e874078728b3bf584d31c8cf2f6762c108ee
demeritbird <lekjiewei@hotmail.sg>
2023-03-02 22:12:36 +0800

    test: patch naive test case for profilepage

--- client/src/routes/ProfilePage/ProfilePage.test.tsx
+++ client/src/routes/ProfilePage/ProfilePage.test.tsx
@@ -1,1 +1,8 @@
-export {}
+import { render, screen } from '../../utils/testing-library-utils';
+import ProfilePage from './ProfilePage';
+
+test('sample render test value for ProfilePage', () => {
+  render(<ProfilePage />);
+  const pandingPageTestText = screen.getByText(/Profile Page/i);
+  expect(pandingPageTestText).toBeInTheDocument();
+});
--- client/src/routes/ProfilePage/ProfilePage.tsx
+++ client/src/routes/ProfilePage/ProfilePage.tsx
@@ -1,28 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, Fragment } from 'react';
 import useAxios from '../../hooks/useAxios';
 import styles from './ProfilePage.module.scss';
 
 interface ProfileDataType {
-  email:string
-  name: string
+  email: string;
+  name: string;
 }
 
 function ProfilePage() {
-  const { response, error, loading,  axiosRequest } = useAxios();
+  const { response, error, loading, axiosRequest } = useAxios();
   async function getProfileData() {
     axiosRequest({
       method: 'get',
-      url: 'api/v1/users/me'
+      url: 'api/v1/users/me',
     });
   }
 
   useEffect(() => {
     getProfileData();
-  },[])
+  }, []);
 
-  const profileData: ProfileDataType = response ? response!.data.data : null
+  const profileData: ProfileDataType = response ? response!.data.data : null;
 
-  return <div>{response? profileData.name : 'no profile'}</div>;
+  return (
+    <Fragment>
+      <h2>Profile Page</h2>
+      <div>{response ? profileData.name : 'no profile'}</div>
+    </Fragment>
+  );
 }
 
 export default ProfilePage;
