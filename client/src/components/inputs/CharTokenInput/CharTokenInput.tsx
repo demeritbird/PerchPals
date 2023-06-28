baaad1d8b6b965db8fb9ad4acd2231c64a9ce681
demeritbird <lekjiewei@hotmail.sg>
2023-06-28 21:29:24 +0800

    feat: revamp implementation of token input func in activate

--- client/src/components/inputs/CharTokenInput/CharTokenInput.tsx
+++
@@ -1,5 +0,0 @@
-function CharTokenInput() {
-  return <input type='text' className='input' pattern='[0-9a-zA-Z]' required />;
-}
-
-export default CharTokenInput;
--- client/src/routes/ActivatePage/ActivatePage.tsx
+++ client/src/routes/ActivatePage/ActivatePage.tsx
@@ -1,61 +1,121 @@
-import { FormEvent, useEffect } from 'react';
-import CharTokenInput from '../../components/inputs/CharTokenInput/CharTokenInput';
+import React, { useState, useMemo } from 'react';
 
-// TODO: transform in react-like code.
-// - Link: https://codepen.io/solygambas/pen/KKgYZWR
+// - Taken from: https://github.com/dominicarrojado/react-typescript-otp-input
 function ActivatePage() {
-  useEffect(() => {
-    const inputs = document.querySelectorAll<HTMLInputElement>('.input');
+  const TOKEN_MAX_LENGTH = 6;
+  const ALPHA_NUMERIC_REG = /^[0-9a-zA-Z]$/;
+  const [token, setToken] = useState('');
 
-    if (inputs[0]) inputs[0].focus();
+  const valueItems = useMemo(() => {
+    const valueArray = token.split('');
+    const inputCharArr: Array<string> = [];
 
-    inputs.forEach((input, index) => {
-      input.addEventListener('keydown', (e) => {
-        if (/^[0-9a-zA-Z]$/.test(e.key)) {
-          inputs[index].value = '';
-          setTimeout(() => {
-            if (index + 1 < 6) inputs[index + 1].focus();
-          }, 10);
-        } else if (e.key === 'Backspace') {
-          setTimeout(() => {
-            inputs[index - 1].focus();
-          }, 10);
-        }
-      });
-    });
-  }, []);
+    for (let i = 0; i < TOKEN_MAX_LENGTH; i++) {
+      const inputChar = valueArray[i];
 
-  function onSubmitHandler(event: FormEvent): void {
-    event.preventDefault();
-    const inputs = document.querySelectorAll<HTMLInputElement>('.input');
+      if (ALPHA_NUMERIC_REG.test(inputChar)) {
+        inputCharArr.push(inputChar);
+      } else {
+        inputCharArr.push('');
+      }
+    }
 
-    let combinedToken: string = '';
-    inputs.forEach((input) => {
-      combinedToken += input.value;
-    });
+    return inputCharArr;
+  }, [token]);
 
-    console.log(combinedToken);
+  function focusToPrevInput(target: HTMLElement) {
+    const prevElementSibling = target.previousElementSibling as HTMLInputElement | null;
+    if (prevElementSibling) {
+      prevElementSibling.focus();
+    }
+  }
+  function focusToNextInput(target: HTMLElement) {
+    const nextElementSibling = target.nextElementSibling as HTMLInputElement | null;
+    if (nextElementSibling) {
+      nextElementSibling.focus();
+    }
+  }
+
+  function inputOnChange(event: React.ChangeEvent<HTMLInputElement>, idx: number) {
+    let targetValue = event.target.value.trim();
+    const isTargetValueChar = ALPHA_NUMERIC_REG.test(targetValue);
+
+    if (!isTargetValueChar && targetValue !== '') {
+      return;
+    }
+    const nextInputEl = event.target.nextElementSibling as HTMLInputElement | null;
+    // only delete char if next input element has no value
+    if (!isTargetValueChar && nextInputEl && nextInputEl.value !== '') {
+      return;
+    }
+
+    targetValue = isTargetValueChar ? targetValue : ' ';
+    const targetTokenTotalLength = targetValue.length;
+
+    if (targetTokenTotalLength === 1) {
+      const newValue = token.substring(0, idx) + targetValue + token.substring(idx + 1);
+      setToken(newValue);
+
+      if (!isTargetValueChar) {
+        return;
+      }
+
+      focusToNextInput(event.target);
+    } else if (targetTokenTotalLength === TOKEN_MAX_LENGTH) {
+      setToken(targetValue);
+
+      event.target.blur();
+    }
+  }
+  function inputOnKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    const target = event.target as HTMLInputElement;
+
+    if (event.key === 'ArrowLeft') {
+      event.preventDefault();
+      return focusToPrevInput(target);
+    }
+    if (event.key === 'ArrowRight') {
+      event.preventDefault();
+      return focusToNextInput(target);
+    }
+
+    const targetValue = target.value;
+
+    // keep the selection range position if the same char was typed
+    target.setSelectionRange(0, targetValue.length);
+
+    if (event.key !== 'Backspace' || targetValue !== '') {
+      return;
+    }
+
+    focusToPrevInput(target);
+  }
+  function inputOnFocus(event: React.FocusEvent<HTMLInputElement>) {
+    // keep focusing back until previous input element has value
+    const prevInputEl = event.target.previousElementSibling as HTMLInputElement | null;
+    if (prevInputEl && prevInputEl.value === '') {
+      return prevInputEl.focus();
+    }
+
+    event.target.setSelectionRange(0, event.target.value.length);
   }
 
   return (
-    <div className='container'>
-      <h2>Activate Your Account</h2>
-      <p>Check your email at: example.com</p>
-      <div className='input-container'>
-        <form
-          onSubmit={(event: FormEvent) => onSubmitHandler(event)}
-          encType='multipart/form-data'
-        >
-          <CharTokenInput />
-          <CharTokenInput />
-          <CharTokenInput />
-          <CharTokenInput />
-          <CharTokenInput />
-          <CharTokenInput />
-
-          <button type='submit'>submit token</button>
-        </form>
-      </div>
+    <div>
+      {valueItems.map((char, idx) => (
+        <input
+          key={idx}
+          type='text'
+          inputMode='text'
+          autoComplete='one-time-code'
+          pattern='\d{1}'
+          maxLength={TOKEN_MAX_LENGTH}
+          value={char}
+          onChange={(e) => inputOnChange(e, idx)}
+          onKeyDown={inputOnKeyDown}
+          onFocus={inputOnFocus}
+        />
+      ))}
     </div>
   );
 }
