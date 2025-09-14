d20d3d12a88c5c07fc4a525dfad466c8d437b359
demeritbird <lekjiewei@hotmail.sg>
2025-09-14 20:03:39 +0800

    refactor: requireAuth cases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 
 import './index.scss'; // global stylesheet
@@ -7,8 +7,6 @@ import styles from './App.module.scss';
 import RequireAuth from './routes/RequireAuth';
 import PersistLogin from './routes/PersistLogin';
 
-import { UserRoles } from '@backend/types';
-
 import AuthPage from './routes/AuthPage';
 const ActivatePage = React.lazy(() => import('./routes/ActivatePage'));
 const LandingPage = React.lazy(() => import('./routes/LandingPage'));
@@ -16,58 +14,56 @@ const ProfilePage = React.lazy(() => import('./routes/ProfilePage'));
 
 function App() {
   return (
-    <Fragment>
-      <Routes>
-        <Route element={<PersistLogin />}>
-          {/* Onboarding Layer */}
-          <Route
-            path='/'
-            element={
-              <Suspense fallback={<div>loading...</div>}>
-                <div>Dashboard Page</div>
-              </Suspense>
-            }
-          />
+    <Routes>
+      <Route element={<PersistLogin />}>
+        {/* Onboarding Layer */}
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<div>loading...</div>}>
+              <div>Dashboard Page</div>
+            </Suspense>
+          }
+        />
 
-          {/* Application Layer */}
-          <Route
-            element={
-              <div className={`${styles.container}`}>
-                <Outlet />
-              </div>
-            }
-          >
-            {/* Authentication Pages */}
-            <Route path='/auth' element={<AuthPage />}></Route>
-            <Route element={<RequireAuth allowedRoles={[UserRoles.USER, UserRoles.ADMIN]} />}>
-              <Route path='/auth/activate' element={<ActivatePage />} />
-            </Route>
-
-            {/* Dashboard Pages */}
-            <Route element={<RequireAuth allowedRoles={[UserRoles.USER, UserRoles.ADMIN]} />}>
-              <Route
-                path='/landingpage'
-                element={
-                  <Suspense fallback={<div>Loading...</div>}>
-                    <LandingPage />
-                  </Suspense>
-                }
-              />
-              <Route
-                path='/profilepage'
-                element={
-                  <Suspense fallback={<div>Loading...</div>}>
-                    <ProfilePage />
-                  </Suspense>
-                }
-              />
-            </Route>
+        {/* Application Layer */}
+        <Route
+          element={
+            <div className={`${styles.container}`}>
+              <Outlet />
+            </div>
+          }
+        >
+          {/* Authentication Pages */}
+          <Route path='/auth' element={<AuthPage />}></Route>
+          <Route element={<RequireAuth />}>
+            <Route path='/auth/activate' element={<ActivatePage />} />
           </Route>
 
-          <Route path='/*' element={<Navigate to='/auth' replace />} />
+          {/* Dashboard Pages */}
+          <Route element={<RequireAuth />}>
+            <Route
+              path='/landingpage'
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LandingPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path='/profilepage'
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ProfilePage />
+                </Suspense>
+              }
+            />
+          </Route>
         </Route>
-      </Routes>
-    </Fragment>
+
+        <Route path='/*' element={<Navigate to='/auth' replace />} />
+      </Route>
+    </Routes>
   );
 }
 
--- client/src/routes/RequireAuth.tsx
+++ client/src/routes/RequireAuth.tsx
@@ -2,27 +2,44 @@ import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import useAuth from './../hooks/useAuth';
 import { AccountStatus, UserRoles } from '@backend/types';
 
-interface AllowedRolesProps {
-  allowedRoles: Array<UserRoles>;
+import { Fragment } from 'react';
+
+interface RequireAuthProps {
+  allowedRoles?: Array<UserRoles>;
 }
-function RequireAuth(props: AllowedRolesProps) {
-  const { allowedRoles } = props;
+
+/**
+ * based on a user's status within the application,
+ * this wrapper determines which page the user shall be routed to.
+ * @example  <Route element={<RequireAuth allowedRoles={[UserRoles.x, ...]} />}>
+               <Route ... />
+             </Route>
+ */
+function RequireAuth(props: RequireAuthProps) {
+  // allow access to all roles if no props provided.
+  const { allowedRoles = Object.values(UserRoles) } = props;
+
   const { authUser } = useAuth();
   const location = useLocation();
+  const locationSegments = location.pathname.split('/').filter(Boolean);
 
-  console.log('====');
-  console.log(authUser);
+  function navigateToRoute(): JSX.Element {
+    // user is not authenticated
+    if (!authUser) return <Navigate to='/auth' state={{ from: location }} replace />;
+    // user is authenticated but has not activated their account
+    if (authUser.active === AccountStatus.PENDING)
+      return <Navigate to='/auth/activate' state={{ from: location }} replace />;
+    // user has already authenticated and activated account but chooses to go to activate page.
+    // OR user has no role-access to this page
+    // NOTE: landing page should be open to all roles.
+    if (locationSegments.at(-1) === 'activate' || !allowedRoles.includes(authUser.role))
+      return <Navigate to='/landingpage' state={{ from: location }} replace />;
 
-  return authUser && authUser.active === AccountStatus.PENDING ? (
-    <Navigate to='/activate' />
-  ) : authUser &&
-    authUser.active === AccountStatus.ACTIVE &&
-    allowedRoles.includes(authUser.role) &&
-    location.pathname !== '/activate' ? (
-    <Outlet />
-  ) : (
-    <Navigate to='/auth' state={{ from: location }} replace />
-  );
+    // otherwise they should navigate to intended route
+    return <Outlet />;
+  }
+
+  return <Fragment>{navigateToRoute()}</Fragment>;
 }
 
 export default RequireAuth;
